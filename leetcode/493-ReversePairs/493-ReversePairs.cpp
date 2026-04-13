aedba476da51796febcf3454d7314f9df854355d
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:36:46 +0530

    Create 493-ReversePairs

---
+++ leetcode/493-ReversePairs/493-ReversePairs.cpp
@@ -0,0 +1,43 @@
+// Last updated: 4/13/2026, 3:36:44 PM
+class Solution {
+public:
+    void help(vector<int>& nums,int i,int j,int& a){
+        if(i==j) return;
+        int d=(i+j)/2;
+        help(nums,i,d,a);
+        help(nums,d+1,j,a);
+        vector<int>b(j-i+1);
+        int k=i,l=d+1;
+        while(k<=d&&l<=j){
+            if(2*1LL*nums[l]<nums[k]){
+                a+=d-k+1;
+                l++;
+            }else k++;
+        }
+        k=i;
+        l=d+1;
+        int m=0;
+        while(k<=d&&l<=j){
+            if(nums[k]<=nums[l]){
+                b[m++]=nums[k++];
+            }else{
+                b[m++]=nums[l++];
+            }
+        }
+        while(k<=d){
+            b[m++]=nums[k++];
+        }
+        while(l<=j){
+            b[m++]=nums[l++];
+        }
+        for(m=0;m<j-i+1;m++){
+            nums[i+m]=b[m];
+        }
+    }
+    int reversePairs(vector<int>& nums) {
+        int a=0;
+        int n=nums.size();
+        help(nums,0,n-1,a);
+        return a;
+    }
+};
