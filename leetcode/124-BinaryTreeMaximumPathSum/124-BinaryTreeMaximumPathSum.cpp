2a6fc7b9faffdb1c7c8af91d307287ad79cab622
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:39:12 +0530

    Create 124-BinaryTreeMaximumPathSum

---
+++ leetcode/124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.cpp
@@ -0,0 +1,28 @@
+// Last updated: 4/13/2026, 3:39:10 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int mx= INT_MIN;
+    int help(TreeNode* root){
+        if(root==NULL) return 0;
+        int l=help(root->left),r=help(root->right);
+        if(root->left) mx=max(mx,l);
+        if(root->right) mx=max(mx,r);
+        mx=max({mx,r+l+root->val});
+        return max(root->val+max(r,l),root->val);
+    }
+    int maxPathSum(TreeNode* root) {
+        mx=INT_MIN;
+        return max(mx,help(root));
+    }
+};
