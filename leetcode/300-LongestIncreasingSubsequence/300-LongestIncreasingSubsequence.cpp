46a12c11a9356a1d7b7233a756b79ec659f1d1ad
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:37:29 +0530

    Create 300-LongestIncreasingSubsequence

---
+++ leetcode/300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.cpp
@@ -0,0 +1,19 @@
+// Last updated: 4/13/2026, 3:37:27 PM
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>a;
+    int m=0;
+    for(int i=0;i<n;i++){
+        if(m==0||m>0&&a[m-1]<nums[i]) {a.push_back(nums[i]);m++;}
+        else {
+            auto it=lower_bound(a.begin(), a.end(),nums[i]);
+            if (it != a.end()) {
+               *it = nums[i];
+            }
+        }
+    }
+    return m;
+    }
+};
