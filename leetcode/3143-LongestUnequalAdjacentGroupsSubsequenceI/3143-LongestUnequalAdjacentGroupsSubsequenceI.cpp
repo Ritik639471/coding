986921c375a54223e65adfee83ce33c00170fc3b
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:32:32 +0530

    Create 3143-LongestUnequalAdjacentGroupsSubsequenceI

---
+++ leetcode/3143-LongestUnequalAdjacentGroupsSubsequenceI/3143-LongestUnequalAdjacentGroupsSubsequenceI.cpp
@@ -0,0 +1,13 @@
+// Last updated: 4/13/2026, 3:32:30 PM
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words, vector<int>& groups) {
+        int n=words.size();
+        vector<string> ans;
+        ans.push_back(words[0]);
+        for(int i=1;i<n;i++){
+            if(groups[i]!=groups[i-1]) ans.push_back(words[i]);
+        }
+        return ans;
+    }
+};
