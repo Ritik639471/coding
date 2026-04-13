2c63d5978037c143fe408d6ff95f502b8e15274d
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:35:48 +0530

    Create 739-DailyTemperatures

---
+++ leetcode/739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/13/2026, 3:35:46 PM
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) {
+        stack<int>q;
+        int n=t.size();
+        vector<int>ans(n,0);
+        for(int i=0;i<n;i++){
+            while(!q.empty()&&t[i]>t[q.top()]){
+                ans[q.top()]=i-q.top();
+                q.pop();
+            }
+            q.push(i);
+        }
+        return ans;
+    }
+};
