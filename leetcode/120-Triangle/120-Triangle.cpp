cf8978226af3fb37486ef713577243568c2414b7
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:39:18 +0530

    Create 120-Triangle

---
+++ leetcode/120-Triangle/120-Triangle.cpp
@@ -0,0 +1,13 @@
+// Last updated: 4/13/2026, 3:39:16 PM
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
+            }
+        }  
+        return triangle[0][0];
+    }
+};
