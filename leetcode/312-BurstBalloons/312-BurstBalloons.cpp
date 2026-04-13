ad1754b404b97dd9ff78d33a7a8dd972db1656c5
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:37:24 +0530

    Create 312-BurstBalloons

---
+++ leetcode/312-BurstBalloons/312-BurstBalloons.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/13/2026, 3:37:22 PM
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n=nums.size();
+        nums.insert(nums.begin(),1);
+        nums.push_back(1);
+        int dp[302][302] = {0};
+        for(int i=n;i>=1;i--){
+            for(int j=i;j<=n;j++){
+                int cost=0;
+                for(int k=i;k<=j;k++){
+                    cost=max(cost,nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j]);
+                }
+                dp[i][j]=cost;
+            }
+        }
+        return dp[1][n];
+    }
+};
