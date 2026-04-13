9e338b0e856c20e6a08eb44bcb0c6786128e6bcf
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:33:04 +0530

    Create 2392-SuccessfulPairsOfSpellsAndPotions

---
+++ leetcode/2392-SuccessfulPairsOfSpellsAndPotions/2392-SuccessfulPairsOfSpellsAndPotions.cpp
@@ -0,0 +1,23 @@
+// Last updated: 4/13/2026, 3:33:02 PM
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        int n=spells.size(),m=potions.size(),mid;
+        sort(potions.begin(),potions.end());
+        for(int i=0;i<n;i++){
+            int lo=0,hi=m-1;
+            int a=0;
+            while(lo<=hi){
+                mid=lo+(hi-lo)/2;
+                if((long long)spells[i]*(long long)potions[mid]>=success){
+                    hi=mid-1;
+                    a=m-mid;
+                }else{
+                    lo=mid+1;
+                }
+            }
+            spells[i]=a;
+        }
+        return spells;
+    }
+};
