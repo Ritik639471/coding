4d8eb90b29ca84b8151646cbc0093e4898f34b8f
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:32:35 +0530

    Create 2882-WaysToExpressAnIntegerAsSumOfPowers

---
+++ leetcode/2882-WaysToExpressAnIntegerAsSumOfPowers/2882-WaysToExpressAnIntegerAsSumOfPowers.cpp
@@ -0,0 +1,19 @@
+// Last updated: 4/13/2026, 3:32:33 PM
+class Solution {
+public:
+    int m=1e9+7;
+    int numberOfWays(int n, int x) {
+        vector<long long>a(n+1,0);
+        a[0]=1;
+        long long ans=0;
+        for(long i=n;i>=1;i--){
+            long long l=pow(i,x);
+            for(int j=n-1;j>=0;j--){
+                if(a[j]&&(j+l)<=n){
+                    a[j+l]+=(a[j]);
+                }
+            }
+        }
+        return a[n]%m;
+    }
+};
