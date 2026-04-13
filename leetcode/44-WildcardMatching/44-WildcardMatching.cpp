ec2c8bfd13ccefbc319b1d597c7e5e6980b6801c
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:40:52 +0530

    Create 44-WildcardMatching

---
+++ leetcode/44-WildcardMatching/44-WildcardMatching.cpp
@@ -0,0 +1,25 @@
+// Last updated: 4/13/2026, 3:40:50 PM
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n=s.size(),m=p.size();
+        vector<bool>dp(m+1,false),dp1;
+        dp[m]=true;
+        for(int j=m-1;j>=0;j--){
+            if(p[j]=='*') dp[j]=dp[j+1];
+        }
+        for(int i=n-1;i>=0;i--){
+            dp1=dp;
+            bool prev=(i==n-1);
+            dp[m]=false;
+            for(int j=m-1;j>=0;j--){
+                bool cur=dp[j];
+                dp[j]=false;
+                if(s[i]==p[j]||p[j]=='?') dp[j]=prev;
+                else if(p[j]=='*') dp[j]=prev|cur|dp[j+1];
+                prev=cur;
+            }
+        }
+        return dp[0];	
+    }
+};
