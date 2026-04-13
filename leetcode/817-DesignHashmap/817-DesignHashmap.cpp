741e1fe39b77af6c15e9e35ccd2521bc766ba77c
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:35:24 +0530

    Create 817-DesignHashmap

---
+++ leetcode/817-DesignHashmap/817-DesignHashmap.cpp
@@ -0,0 +1,29 @@
+// Last updated: 4/13/2026, 3:35:22 PM
+class MyHashMap {
+public:
+    map<int,int> m;
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        m[key]=value;
+    }
+    
+    int get(int key) {
+        if(m.find(key)==m.end())return -1;
+        return m[key];
+    }
+    
+    void remove(int key) {
+        m.erase(key);
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
