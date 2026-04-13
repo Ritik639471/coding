ecc10bb315addd7dc5b50423271865a8aa9db700
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:39:38 +0530

    Create 101-SymmetricTree

---
+++ leetcode/101-SymmetricTree/101-SymmetricTree.cpp
@@ -0,0 +1,23 @@
+// Last updated: 4/13/2026, 3:39:36 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool is(TreeNode* p, TreeNode* q) {
+        if(!p&&!q) return true;
+        if(!p||!q) return false;
+        return (p->val==q->val)&&is(p->left,q->right)&&is(p->right,q->left);
+    }
+    bool isSymmetric(TreeNode* root) {
+        return is(root->left,root->right);
+    }
+};
