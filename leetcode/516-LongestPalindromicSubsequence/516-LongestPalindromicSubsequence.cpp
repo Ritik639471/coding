c83e3fa43fcd124f27b720353fbc84734ca9709c
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:36:40 +0530

    Create 516-LongestPalindromicSubsequence

---
+++ leetcode/516-LongestPalindromicSubsequence/516-LongestPalindromicSubsequence.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/13/2026, 3:36:38 PM
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n=s.length();
+        vector<int>a(n+1,0),b;
+        for(int i=1;i<=n;i++){
+            b=a;
+            for(int j=1;j<=n;j++){
+                if(s[i-1]==s[n-j]){
+                    a[j]=1+b[j-1];
+                }
+                a[j]=max({b[j],a[j-1],a[j]});
+            }
+        }
+        return a[n];
+    }
+};
