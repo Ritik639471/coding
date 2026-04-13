5dcbee3fbb4819f4d81b929f3f4fd2f91cf60811
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:38:06 +0530

    Create 216-CombinationSumIii

---
+++ leetcode/216-CombinationSumIii/216-CombinationSumIii.cpp
@@ -0,0 +1,23 @@
+// Last updated: 4/13/2026, 3:38:04 PM
+class Solution {
+public:
+    void help(vector<vector<int>>&ans,int k,int n,vector<int>&a,int i){
+        if(k<0||n<0) return;
+        if(k==0&&n==0) {
+            ans.push_back(a);
+            return;
+        }
+        else if(k==0||n==0) return;
+        for(int j=i;j<=9;j++){
+            vector<int> b=a;
+            b.push_back(j);
+            help(ans,k-1,n-j,b,j+1);
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>>ans;
+        vector<int>a;
+        help(ans,k,n,a,1);
+        return ans;
+    }
+};
