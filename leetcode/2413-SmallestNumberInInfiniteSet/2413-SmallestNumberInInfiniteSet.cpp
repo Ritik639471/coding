2a76804916dba27181b41cc2e66256a2db722aa5
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:33:00 +0530

    Create 2413-SmallestNumberInInfiniteSet

---
+++ leetcode/2413-SmallestNumberInInfiniteSet/2413-SmallestNumberInInfiniteSet.cpp
@@ -0,0 +1,28 @@
+// Last updated: 4/13/2026, 3:32:58 PM
+class SmallestInfiniteSet {
+public:
+    priority_queue<int,vector<int>,greater<int>>pq;
+    unordered_map<int,bool>m;
+    SmallestInfiniteSet() {
+        for(int i=1;i<=1000;i++) {pq.push(i);m[i]=true;}
+    }
+    
+    int popSmallest() {
+        int i=pq.top();
+        pq.pop();
+        m[i]=false;
+        return i;
+    }
+    
+    void addBack(int num) {
+        if(!m[num]) pq.push(num);
+        m[num]=true;
+    }
+};
+
+/**
+ * Your SmallestInfiniteSet object will be instantiated and called as such:
+ * SmallestInfiniteSet* obj = new SmallestInfiniteSet();
+ * int param_1 = obj->popSmallest();
+ * obj->addBack(num);
+ */
