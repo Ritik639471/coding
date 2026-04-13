49ee354d00abd64a55170073ef9654e216369d01
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:38:02 +0530

    Create 225-ImplementStackUsingQueues

---
+++ leetcode/225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.cpp
@@ -0,0 +1,42 @@
+// Last updated: 4/13/2026, 3:38:00 PM
+class MyStack {
+private:
+    queue<int>stack;
+public:
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        stack.push(-1);
+        stack.push(x);
+        while(stack.front()!=-1){
+            stack.push(stack.front());
+            stack.pop();
+        }
+        stack.pop();
+    }
+    
+    int pop() {
+        int n=stack.front();
+        stack.pop();
+        return n;
+    }
+    
+    int top() {
+        return stack.front();
+    }
+    
+    bool empty() {
+        return stack.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
