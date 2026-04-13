a6169c894a959c79d0c77baa2971ab66151cd1ad
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:35:27 +0530

    Create 813-AllPathsFromSourceToTarget

---
+++ leetcode/813-AllPathsFromSourceToTarget/813-AllPathsFromSourceToTarget.cpp
@@ -0,0 +1,24 @@
+// Last updated: 4/13/2026, 3:35:25 PM
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>>ans;
+        int n=graph.size();
+        vector<int>v;
+        queue<pair<int,vector<int>>>q;
+        q.push({0,v});
+        while(!q.empty()){
+            auto d=q.front();
+            q.pop();
+            d.second.push_back(d.first);
+            if(d.first==n-1){
+                ans.push_back(d.second);
+                continue;
+            }
+            for(auto c:graph[d.first]){
+                q.push({c,d.second});
+            }
+        }
+        return ans;
+    }
+};
