f0893a57dd5555d3a3427a81cef378c1486882d6
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:35:59 +0530

    Create 714-BestTimeToBuyAndSellStockWithTransactionFee

---
+++ leetcode/714-BestTimeToBuyAndSellStockWithTransactionFee/714-BestTimeToBuyAndSellStockWithTransactionFee.cpp
@@ -0,0 +1,13 @@
+// Last updated: 4/13/2026, 3:35:57 PM
+class Solution {
+public:
+    int maxProfit(vector<int>& prices, int fee) {
+        int n=prices.size();
+        vector<int>dp(n+2,0),dp1(n+2,0);
+        for(int i=n-1;i>=0;i--){
+            dp[i]=max({-prices[i]+dp1[i+1],dp[i+1]});
+            dp1[i]=max({prices[i]+dp[i+1]-fee,dp1[i+1]});
+        }
+        return dp[0];
+    }
+};
