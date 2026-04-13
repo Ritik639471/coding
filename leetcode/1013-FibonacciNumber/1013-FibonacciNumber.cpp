1d75c2f44267dc77a11fc4fdc6cd1e157d30428f
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:34:48 +0530

    Create 1013-FibonacciNumber

---
+++ leetcode/1013-FibonacciNumber/1013-FibonacciNumber.cpp
@@ -0,0 +1,14 @@
+// Last updated: 4/13/2026, 3:34:46 PM
+class Solution {
+public:
+    int fib(int n) {
+       vector<int> dp(n+1);
+       for(int i=0;i<=n;i++){
+         if(i==0||i==1){dp[i]=i;}
+         else{
+            dp[i]=dp[i-1]+dp[i-2];
+         }
+       }
+       return dp[n];
+    }
+};
