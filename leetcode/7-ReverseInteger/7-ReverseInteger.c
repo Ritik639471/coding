f1a6b06ff9106c3368b5ff9dd397e76bacfbc7ab
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:41:54 +0530

    Create 7-ReverseInteger

---
+++ leetcode/7-ReverseInteger/7-ReverseInteger.c
@@ -0,0 +1,14 @@
+// Last updated: 4/13/2026, 3:41:52 PM
+#include<limits.h>
+int reverse(int x){
+    int temp=0;
+    while(x!=0){
+        if(temp<INT_MIN/10||temp>INT_MAX/10)
+           return 0;
+        else{
+         temp=temp*10;
+         temp+=x%10;
+         x=x/10;}
+    }
+    return temp;
+}
