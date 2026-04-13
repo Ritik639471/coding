9daf1d6f96787b4023c3282e5029d1de0332acec
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:36:37 +0530

    Create 538-ConvertBstToGreaterTree

---
+++ leetcode/538-ConvertBstToGreaterTree/538-ConvertBstToGreaterTree.cpp
@@ -0,0 +1,27 @@
+// Last updated: 4/13/2026, 3:36:35 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int i=0;
+    void helper(TreeNode* root){
+        if(root==NULL) return;
+        helper(root->right);
+        root->val+=i;
+        i=root->val;
+        helper(root->left);
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        helper(root);
+        return root;
+    }
+};
