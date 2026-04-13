ce439346347ffd586853e725c666ba6a33a852df
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:36:30 +0530

    Create 560-SubarraySumEqualsK

---
+++ leetcode/560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/13/2026, 3:36:28 PM
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size(),count=0;
+        int sum=0;
+        map<int,int> m;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum==k) count++;
+            if(m[sum-k]){
+                count+=m[sum-k];
+            }
+            m[sum]++;
+        }
+        return count;
+    }
+};
