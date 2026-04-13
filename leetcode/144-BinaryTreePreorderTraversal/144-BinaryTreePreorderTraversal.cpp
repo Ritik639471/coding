930118e8e52ecd25da686b7ac02e30a7fde8b94c
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:38:49 +0530

    Create 144-BinaryTreePreorderTraversal

---
+++ leetcode/144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,29 @@
+// Last updated: 4/13/2026, 3:38:48 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void helper(TreeNode* root,vector<int>& v){
+        if(root==NULL) return;
+        v.push_back(root->val);
+        helper(root->left,v);
+        helper(root->right,v);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        vector<int> v;
+        helper(root,v);
+        return v;
+    }
+};
