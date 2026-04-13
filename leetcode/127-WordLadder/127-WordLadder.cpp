415e01f9b38f134f614043bad30eea3c85bec93f
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:39:09 +0530

    Create 127-WordLadder

---
+++ leetcode/127-WordLadder/127-WordLadder.cpp
@@ -0,0 +1,30 @@
+// Last updated: 4/13/2026, 3:39:07 PM
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(0);
+        unordered_map<string,bool>mp;
+        for(auto c: wordList){
+            mp[c]=true;
+        }
+        queue<pair<string,int>>rq;
+        rq.push({beginWord,1});
+        while(!rq.empty()){
+            auto c=rq.front();
+            rq.pop();
+            for(int i=0;i<c.first.length();i++){
+                for(char j='a';j<='z';j++){
+                    string s=c.first;
+                    s[i]=j;
+                    if(mp[s]&&s==endWord) return c.second+1;
+                    if(mp[s]){
+                        rq.push({s,c.second+1});
+                        mp[s]=false;
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
