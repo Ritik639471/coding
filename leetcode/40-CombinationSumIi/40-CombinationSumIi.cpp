50097286672e233ca6f24b65e812e3af3d3e7f62
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:40:57 +0530

    Create 40-CombinationSumIi

---
+++ leetcode/40-CombinationSumIi/40-CombinationSumIi.cpp
@@ -0,0 +1,24 @@
+// Last updated: 4/13/2026, 3:40:55 PM
+class Solution {
+public:
+    void helper(vector<vector<int>>& ans,vector<int>& v,int idx,int target,vector<int>& num){
+      if(target==0) {
+        ans.push_back(v);
+        return;
+      }
+      for(int i=idx;i<num.size();i++){
+        if(i>idx&&num[i-1]==num[i]) continue;
+        if(target<num[i]||target-num[i]<0) return;
+        v.push_back(num[i]);
+        helper(ans,v,i+1,target-num[i],num);
+        v.pop_back();
+      }
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> v;
+        helper(ans,v,0,target,candidates);
+        return ans;
+    }
+};
