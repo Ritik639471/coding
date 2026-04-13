470951e538028a6d7a63e57ac3a3877effcc09ea
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:37:01 +0530

    Create 416-PartitionEqualSubsetSum

---
+++ leetcode/416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.cpp
@@ -0,0 +1,19 @@
+// Last updated: 4/13/2026, 3:36:59 PM
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n=nums.size(),sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        if(sum%2==1) return false;
+        vector<int>dp((sum/2)+1,false);
+        dp[0]=true;
+        for(int i=0;i<n;i++){
+            for(int j=sum/2;j>=0;j--){
+                if(dp[j]&&nums[i]+j<=sum/2) dp[nums[i]+j]=true;
+            }
+        }
+        return dp[sum/2];
+    }
+};
