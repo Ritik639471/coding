55fc8c3f4a9a70ebd67fe572c561c9cdc487907a
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:37:55 +0530

    Create 230-KthSmallestElementInABst

---
+++ leetcode/230-KthSmallestElementInABst/230-KthSmallestElementInABst.cpp
@@ -0,0 +1,27 @@
+// Last updated: 4/13/2026, 3:37:53 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int i=0,ans=0;
+    void help(TreeNode* root, int k){
+        if(!root) return;
+        help(root->left,k);
+        i++;
+        if(i==k) ans=root->val;
+        help(root->right,k);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        help(root,k);
+        return ans;
+    }
+};
