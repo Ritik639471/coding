6101a55d8e26775026c94c6b1d6c6e6fc07a3c42
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:40:03 +0530

    Create 78-Subsets

---
+++ leetcode/78-Subsets/78-Subsets.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/13/2026, 3:40:01 PM
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>ans(pow(2,n));
+        for(int i=0;i<ans.size();i++){
+            vector<int>v;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j)){
+                    v.push_back(nums[j]);
+                }
+            }
+            ans[i]=v;
+        }
+        return ans;
+    }
+};
