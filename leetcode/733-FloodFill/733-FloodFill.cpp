6b3305e744c1ee9b56ce3dd9c367f474b764c41c
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:35:52 +0530

    Create 733-FloodFill

---
+++ leetcode/733-FloodFill/733-FloodFill.cpp
@@ -0,0 +1,28 @@
+// Last updated: 4/13/2026, 3:35:50 PM
+class Solution {
+public:
+    int dir[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    bool valid(int i ,int j ,int n ,int m){
+     return i>=0&&i<n&&j>=0&&j<m;
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m=image.size(),n=image[0].size();
+        if(image[sr][sc]==color) return image;
+        int a=image[sr][sc];
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        while(!q.empty()){
+            auto c=q.front();
+            q.pop();
+            if(image[c.first][c.second]==color) continue;
+            image[c.first][c.second]=color;
+            for(int l=0;l<4;l++){
+                int x=dir[l][0]+c.first,y=dir[l][1]+c.second;
+                if((x>=0&&x<m&&y>=0&&y<n)&&image[x][y]==a){
+                    q.push({x,y});
+                }
+            }
+        }
+        return image;
+    }
+};
