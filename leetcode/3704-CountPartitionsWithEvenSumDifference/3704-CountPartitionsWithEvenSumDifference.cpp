53905dbc55fc7ac5a58ee038e87df0eb22526499
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:31:17 +0530

    Create 3704-CountPartitionsWithEvenSumDifference

---
+++ leetcode/3704-CountPartitionsWithEvenSumDifference/3704-CountPartitionsWithEvenSumDifference.cpp
@@ -0,0 +1,14 @@
+// Last updated: 4/13/2026, 3:31:15 PM
+class Solution {
+public:
+    int countPartitions(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0,ans=0,s=0;
+        s=accumulate(nums.begin(),nums.end(),0);
+        for(int i=0;i<n-1;i++){
+            sum+=nums[i];
+            if(abs(s-2*sum)%2==0) ans++;
+        }
+        return ans;
+    }
+};
