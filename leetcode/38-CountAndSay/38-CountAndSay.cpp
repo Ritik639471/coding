0e4efee3c033915a2efcb1ac8ee6e77a8a40c783
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:41:00 +0530

    Create 38-CountAndSay

---
+++ leetcode/38-CountAndSay/38-CountAndSay.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/13/2026, 3:40:59 PM
+class Solution {
+public:
+    string countAndSay(int n) {
+        string st="1";
+        for(int i=1;i<n;i++){
+            string s;
+            for(int j=0;j<st.length();j++){
+                char c=st[j];
+                int a=1;
+                while(j<st.length()-1&&st[j+1]==c){
+                    j++;
+                    a++;
+                }
+                s+=('0'+a);
+                s+=c;
+            }
+            st=s;
+        }
+        return st;
+    }
+};
