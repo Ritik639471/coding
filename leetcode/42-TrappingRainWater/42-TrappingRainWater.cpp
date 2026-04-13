f99a1cc20e5c9dd7d2e8fccb7f21e322304164d6
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:40:55 +0530

    Create 42-TrappingRainWater

---
+++ leetcode/42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -0,0 +1,27 @@
+// Last updated: 4/13/2026, 3:40:52 PM
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n=height.size();
+        int prev[n];
+        prev[0]=-1;
+        int max=height[0];
+        for(int i=1;i<n;i++){
+            prev[i]=max;
+            if(max<height[i]) max=height[i];
+        }
+        prev[n-1]=-1;
+        max=height[n-1];
+        for(int i=n-2;i>=0;i--){
+            if(max<prev[i]) prev[i]=max;
+            if(max<height[i]) max=height[i];
+        }
+        int water=0;
+        for(int i=1;i<n-1;i++){
+            if(height[i]<prev[i]){
+                water+=(prev[i]-height[i]);
+            }
+        }
+        return water;
+    }
+};
