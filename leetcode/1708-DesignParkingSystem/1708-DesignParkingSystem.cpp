df9d3182a99666a5b092ffe3af1e6c4a7bea37ea
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:33:41 +0530

    Create 1708-DesignParkingSystem

---
+++ leetcode/1708-DesignParkingSystem/1708-DesignParkingSystem.cpp
@@ -0,0 +1,21 @@
+// Last updated: 4/13/2026, 3:33:39 PM
+class ParkingSystem {
+public:
+    int arr[3];
+    ParkingSystem(int big, int medium, int small) {
+        arr[0]=big;
+        arr[1]=medium;
+        arr[2]=small;
+    }
+    
+    bool addCar(int carType) {
+        if(arr[carType-1]>0){arr[carType-1]-=1;return true;}
+        return false;
+    }
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
