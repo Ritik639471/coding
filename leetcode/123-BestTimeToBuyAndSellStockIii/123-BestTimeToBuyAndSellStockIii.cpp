63b161dd6067650778365c7da3e2b54fb12bc1f7
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:39:14 +0530

    Create 123-BestTimeToBuyAndSellStockIii

---
+++ leetcode/123-BestTimeToBuyAndSellStockIii/123-BestTimeToBuyAndSellStockIii.cpp
@@ -0,0 +1,20 @@
+// Last updated: 4/13/2026, 3:39:12 PM
+class Solution {
+public:
+    int maxProfit(vector<int>& A) {
+        int k=2;
+        int n = A.size();
+        vector<int> dp(2*k + 1, 0);
+        for (int i = n-1;i>=0;i--) {
+            for (int l = 1;l<=2*k;l++) {
+                if(l%2==0){
+                    dp[l]=max(dp[l],-A[i]+dp[l-1]);
+                }
+                else{
+                    dp[l]=max(dp[l],A[i]+dp[l-1]);
+                }
+            }
+        }
+        return dp[2*k];
+    }
+};
