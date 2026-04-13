ab05eca1867de02d79a6fb2ce2d5c5a71bc39932
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:36:31 +0530

    Create 547-NumberOfProvinces

---
+++ leetcode/547-NumberOfProvinces/547-NumberOfProvinces.cpp
@@ -0,0 +1,26 @@
+// Last updated: 4/13/2026, 3:36:29 PM
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isC) {
+        int ans=0,n=isC.size(),a;
+        vector<bool>vis(n);
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(vis[i]) continue;
+            vis[i]=true;
+            q.push(i);
+            ans++;
+            while(!q.empty()){
+                a=q.front();
+                q.pop();
+                for(int j=0;j<n;j++){
+                    if(isC[a][j]&&!vis[j]){
+                        q.push(j);
+                        vis[j]=true;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
