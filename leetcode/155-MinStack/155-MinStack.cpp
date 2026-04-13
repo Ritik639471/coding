dc7623de224e21596122900bc17323036588f881
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:38:36 +0530

    Create 155-MinStack

---
+++ leetcode/155-MinStack/155-MinStack.cpp
@@ -0,0 +1,38 @@
+// Last updated: 4/13/2026, 3:38:34 PM
+class MinStack {
+private:
+    stack<pair<int,int>>st;
+public:
+    MinStack() {
+
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            st.push({val,val});
+        }else{
+            st.push({val,min(val,st.top().second)});
+        }
+    }
+    
+    void pop() {
+        st.pop();
+    }
+    
+    int top() {
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
