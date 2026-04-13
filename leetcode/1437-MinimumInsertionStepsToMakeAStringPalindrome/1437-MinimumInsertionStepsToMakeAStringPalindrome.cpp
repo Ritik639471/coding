0abeac5b724d3cc0888c4ab58449de478097a9eb
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:33:58 +0530

    Create 1437-MinimumInsertionStepsToMakeAStringPalindrome

---
+++ leetcode/1437-MinimumInsertionStepsToMakeAStringPalindrome/1437-MinimumInsertionStepsToMakeAStringPalindrome.cpp
@@ -0,0 +1,19 @@
+// Last updated: 4/13/2026, 3:33:56 PM
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n=s.length();
+        vector<int>a(n+1,0),b,c;
+        for(int l=1;l<=n;l++){
+            c=b;
+            b=a;
+            for(int i=0;i+l<=n;i++){
+                int j=i+l-1;
+                if(i==j) a[i]=1;
+                else if(s[i]==s[j]) a[i]=2+c[i+1];
+                else a[i]=max(b[i],b[i+1]);
+            }
+        }
+        return n-a[0];
+    }
+};
