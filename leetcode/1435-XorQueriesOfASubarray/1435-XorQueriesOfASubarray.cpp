6374cf890f6854772f53e6be34d9d7f6d2ed5df2
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:33:59 +0530

    Create 1435-XorQueriesOfASubarray

---
+++ leetcode/1435-XorQueriesOfASubarray/1435-XorQueriesOfASubarray.cpp
@@ -0,0 +1,14 @@
+// Last updated: 4/13/2026, 3:33:57 PM
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int n=arr.size();
+        for(int i=1;i<n;i++) arr[i]=arr[i-1]^arr[i];
+        vector<int>ans(queries.size());
+        for(int i=0;i<queries.size();i++){
+            if(queries[i][0]==0) ans[i]=arr[queries[i][1]];
+            else ans[i]=arr[queries[i][1]]^arr[queries[i][0]-1];
+        }
+        return ans;
+    }
+};
