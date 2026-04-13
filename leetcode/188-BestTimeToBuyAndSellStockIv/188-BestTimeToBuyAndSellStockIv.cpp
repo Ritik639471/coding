4a3196a28dc199173bb503c25abc882fe90f9ee6
Ritik639471 <150527205+Ritik639471@users.noreply.github.com>
2026-04-13 15:38:29 +0530

    Create 188-BestTimeToBuyAndSellStockIv

---
+++ leetcode/188-BestTimeToBuyAndSellStockIv/188-BestTimeToBuyAndSellStockIv.cpp
@@ -0,0 +1,37 @@
+// Last updated: 4/13/2026, 3:38:27 PM
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& A) {
+        // int n = A.size();
+        // vector<int> dp(n + 1, 0),dp1;
+        // for (int l = 1; l <= 2*k; l++) {
+        //     int prev=0;
+        //     for (int i = n - 1; i >= 0; i--) {
+        //         int cur=dp[i];
+        //         if(l%2==0){
+        //             dp[i]=max(dp[i+1],-A[i]+prev);
+        //         }
+        //         else{
+        //             dp[i]=max(dp[i+1],A[i]+prev);
+        //         }
+        //         prev=cur;
+        //         cout<<dp[i]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        // return dp[0];
+        int n = A.size();
+        vector<int> dp(2*k + 1, 0);
+        for (int i = n-1;i>=0;i--) {
+            for (int l = 1;l<=2*k;l++) {
+                if(l%2==0){
+                    dp[l]=max(dp[l],-A[i]+dp[l-1]);
+                }
+                else{
+                    dp[l]=max(dp[l],A[i]+dp[l-1]);
+                }
+            }
+        }
+        return dp[2*k];
+    }
+};
